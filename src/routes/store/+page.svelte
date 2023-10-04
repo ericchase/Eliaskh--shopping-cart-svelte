39c3e9ad9d88f273c97c6754c113340ea18c0b43
ericchase <github@eric.addymail.com>
2023-10-04 12:53:13 -0500

    add store
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/routes/store/+page.svelte
+++ src/routes/store/+page.svelte
@@ -1,5 +1,22 @@
-<script>
+<script lang="ts">
   import products from '$lib/data/product.json';
 
-  console.log(products);
+  let typeOfProduct: string = 'coffee';
+  function handleClink(e: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
+    typeOfProduct = e.currentTarget.value;
+  }
 </script>
+
+<div class="container">
+  <div class="btns">
+    <button class="btn" on:click={handleClink} value="coffee"> Coffee </button>
+    <button class="btn" on:click={handleClink} value="machine"> Machines </button>
+    <button class="btn" on:click={handleClink} value="accessory"> Accessory </button>
+  </div>
+  <div class="cardList">
+    <div class="cards">
+      {products[typeOfProduct][0].name}
+      <!-- {CreateSmallCard(products[typeOfProduct], 10)} -->
+    </div>
+  </div>
+</div>

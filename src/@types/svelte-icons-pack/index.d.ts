b18fdc6edabc92799b3214f956aab3e5b692c38e
ericchase <github@eric.addymail.com>
2023-10-27 00:23:26 -0500

    i think this works as well and fixes an error in tsconfig
    Signed-off-by: ericchase <github@eric.addymail.com>

---
+++ src/@types/svelte-icons-pack/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'svelte-icons-pack/Icon.svelte' {
+  import type { SvelteComponentTyped } from 'svelte';
+  declare class Icon extends SvelteComponentTyped {}
+  export default Icon;
+}
